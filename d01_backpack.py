4c83a51049e2b5770888b6bb634d7f79e07ca7a3
RCBSi <45831224+RCBSi@users.noreply.github.com>
2020-12-03 10:43:33 +0200

    how would it be best to step through a list..

--- d01_backpack.py
+++ d01_backpack.py
@@ -3,9 +3,9 @@ with open('day1.txt', 'r') as file:
 _input.sort()
 min = 0
 max = len(_input) - 1
-while min + max != 2020:
-    if min + max < 2020:
+while _input[min] + _input[max] != 2020:
+    if _input[min] + _input[max] < 2020:
         min += 1
-    if min + max > 2020:
+    if _input[min] + _input[max] > 2020:
         max -= 1
-print(min, max, min*max)
+print(_input[min], _input[max], _input[min]*_input[max])
