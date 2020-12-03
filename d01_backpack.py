70e60a4b3523f1dfab4be92079384185312b4fb3
RCBSi <45831224+RCBSi@users.noreply.github.com>
2020-12-03 10:56:58 +0200

    three-index search the constant-sum surface

--- d01_backpack.py
+++ d01_backpack.py
@@ -1,11 +1,15 @@
 with open('day1.txt', 'r') as file:
     _input = [int(x) for x in file.readlines()]
 _input.sort()
-min = 0
+fix = 0
+min = 1
 max = len(_input) - 1
-while _input[min] + _input[max] != 2020:
-    if _input[min] + _input[max] < 2020:
-        min += 1
-    if _input[min] + _input[max] > 2020:
-        max -= 1
-print(_input[min], _input[max], _input[min]*_input[max])
+while _input[fix] + _input[min] + _input[max] != 2020:
+    min = fix+1
+    max = len(_input) - 1
+    while _input[fix] + _input[min] + _input[max] != 2020:
+        if _input[fix] + _input[min] + _input[max] < 2020:
+            min += 1
+        if _input[fix] + _input[min] + _input[max] > 2020:
+            max -= 1
+print(_input[fix], _input[min], _input[max], _input[fix] * _input[min] * _input[max])
