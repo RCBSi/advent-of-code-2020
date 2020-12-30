44d29eed4c87ccd30ea304eacee63fb2ef17972a
RCBSi <45831224+RCBSi@users.noreply.github.com>
2020-12-30 13:22:34 +0200

    golf

--- day25_private_key_search.py
+++ day25_private_key_search.py
@@ -1,6 +1,3 @@
-pk = [19241437, 17346587] # public keys.
-p1 = 20201227
-
 def find_exponent(desired):
     purepowermodp = 1
     for i in range(p1):
@@ -8,6 +5,6 @@ def find_exponent(desired):
             return i
         purepowermodp = purepowermodp*7%p1
 
-e = find_exponent(pk[0])
-e1 = find_exponent(pk[1])
+[pk,p1] = [[19241437, 17346587],20201227] # public keys, prime modulus.
+(e,e1) = (find_exponent(pk[0]),find_exponent(pk[1]))
 print("part1", 7**(e1*e%(p1-1))%p1)
