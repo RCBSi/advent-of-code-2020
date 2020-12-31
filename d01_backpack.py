a0684c40ff8471390e79e0daf564ef95590f08e8
RCBSi <45831224+RCBSi@users.noreply.github.com>
2020-12-31 17:14:51 +0200

    Update d01_backpack.py

--- d01_backpack.py
+++ d01_backpack.py
@@ -1,15 +1,12 @@
 with open('day1.txt', 'r') as file:
-    _input = [int(x) for x in file.readlines()]
-_input.sort()
-fix = 0
-min = 1
-max = len(_input) - 1
-while _input[fix] + _input[min] + _input[max] != 2020:
-    min = fix+1
-    max = len(_input) - 1
-    while _input[fix] + _input[min] + _input[max] != 2020:
-        if _input[fix] + _input[min] + _input[max] < 2020:
+    n = [int(x) for x in file.readlines()]
+n.sort()
+fix, min, max = (0, 1, len(n)-1)
+while n[fix] + n[min] + n[max] != 2020:
+    min,max = (fix+1, len(n) - 1)
+    while n[fix] + n[min] + n[max] != 2020:
+        if n[fix] + n[min] + n[max] < 2020:
             min += 1
-        if _input[fix] + _input[min] + _input[max] > 2020:
+        if n[fix] + n[min] + n[max] > 2020:
             max -= 1
-print(_input[fix], _input[min], _input[max], _input[fix] * _input[min] * _input[max])
+print(n[fix], n[min], n[max], n[fix] * n[min] * n[max])
